b280fe3dc09c4865c8d0fa9eb40c7372a870304c
duyanning <duyanning@72d5d43e-d8d0-4e3c-8208-7a36d49f3ec9>
2012-01-28 12:41:39 +0000

    add folder for evaluation

---
+++ trunk/ropevm/eval/treeadd-2-1/Tree.java
@@ -0,0 +1,19 @@
+@GroupingPolicies(self=GroupingPolicy.NEW_GROUP)
+class Tree extends TreeNode {
+    Tree(int levels)
+    {
+        super(levels);
+    }
+    void __rvp__ctor(int levels)
+    {
+    }
+
+    // int addTree()
+    // {
+    // }
+    // int _p_slice_for_addTree()
+    // {
+    //     return 0;
+    // }
+
+}
