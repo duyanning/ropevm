7386a59574fbd52715bf912bacbce2df12c08569
duyanning <duyanning@72d5d43e-d8d0-4e3c-8208-7a36d49f3ec9>
2012-03-24 04:58:01 +0000

    refactor

--- trunk/ropevm/SpecMode.h
+++ trunk/ropevm/SpecMode.h
@@ -9,32 +9,30 @@ public:
     SpecMode();
     virtual const char* tag() { return "(S)"; }
 
-    virtual void do_spec_barrier();
-    virtual void do_invoke_method(Object* target_object, MethodBlock* new_mb);
-    virtual void do_method_return(int len);
-    virtual void before_signal_exception(Class *exception_class);
-    virtual void* do_execute_method(Object* target_object, MethodBlock *mb, std::vector<uintptr_t>& jargs, DummyFrame* dummy);
 
-    virtual void do_get_field(Object* obj, FieldBlock* fb, uintptr_t* addr, int size, bool is_static = false);
-    virtual void do_put_field(Object* obj, FieldBlock* fb, uintptr_t* addr, int size, bool is_static = false);
+    void before_signal_exception(Class *exception_class) override;
+    void* do_execute_method(Object* target_object, MethodBlock *mb, std::vector<uintptr_t>& jargs, DummyFrame* dummy) override;
 
-    virtual void do_array_load(Object* array, int index, int type_size);
-    virtual void do_array_store(Object* array, int index, int type_size);
+    void send_msg(Message* msg) override;
+    Frame* push_frame(Object* object, MethodBlock* new_mb, uintptr_t* args,
+                      SpmtThread* caller, CodePntr caller_pc, Frame* caller_frame, uintptr_t* caller_sp,
+                      bool is_top) override;
+    void pop_frame(Frame* frame) override;
 
-    virtual void send_msg(Message* msg);
-
-
-    virtual Frame* push_frame(Object* object, MethodBlock* new_mb, uintptr_t* args,
-                              SpmtThread* caller, CodePntr caller_pc, Frame* caller_frame, uintptr_t* caller_sp,
-                              bool is_top);
-
-    virtual void pop_frame(Frame* frame);
-
-    virtual uint32_t mode_read(uint32_t* addr);
-    virtual void mode_write(uint32_t* addr, uint32_t value);
     //virtual void step();
 
 private:
+    void do_invoke_method(Object* target_object, MethodBlock* new_mb) override;
+    void do_method_return(int len) override;
+    void do_get_field(Object* obj, FieldBlock* fb, uintptr_t* addr, int size, bool is_static = false) override;
+    void do_put_field(Object* obj, FieldBlock* fb, uintptr_t* addr, int size, bool is_static = false) override;
+    void do_array_load(Object* array, int index, int type_size) override;
+    void do_array_store(Object* array, int index, int type_size) override;
+
+    uint32_t mode_read(uint32_t* addr) override;
+    void mode_write(uint32_t* addr, uint32_t value) override;
+
+    void do_spec_barrier() override;
 };
 
 #endif // SPECULATIVEMODE_H
