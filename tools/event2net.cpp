58970725eff613d2ae3e1ab99f9842906d08a24d
Du Yanning <duyanning@gmail.com>
2017-08-05 10:50:03 +0800

    根据两个操作之间的距离确定相关性衰减系数

--- tools/event2net.cpp
+++ tools/event2net.cpp
@@ -7,6 +7,7 @@ struct Vertex {
     string from_object_addr;
     string to_class_name;
     string to_object_addr;
+    intmax_t op_no;
 };
 
 // bool
@@ -20,7 +21,7 @@ struct Vertex {
 struct Edge {
     size_t from;
     size_t to;
-    int weight;
+    double weight;
 };
 
 vector<Vertex> vertices;
@@ -51,6 +52,7 @@ int main(int argc, char* argv[])
     string arrow;
     string target_class_name;
     string target_object_addr;
+    intmax_t op_no;
 
     while (ifs_event) {
         getline(ifs_event, line);
@@ -58,7 +60,7 @@ int main(int argc, char* argv[])
         if (line == "")
             break;
         istringstream iss(line);
-        iss >> source_class_name >> source_object_addr >> arrow >> target_class_name >> target_object_addr;
+        iss >> source_class_name >> source_object_addr >> arrow >> target_class_name >> target_object_addr >> op_no;
         //cout << source_class_name << source_object_addr << arrow << target_class_name << target_object_addr << endl;
 
 
@@ -67,6 +69,7 @@ int main(int argc, char* argv[])
         v.from_object_addr = source_object_addr;
         v.to_class_name = target_class_name;
         v.to_object_addr = target_object_addr;
+        v.op_no = op_no;
         register_vertex(v);
 
     }
@@ -76,35 +79,36 @@ int main(int argc, char* argv[])
         for (size_t j = i+1; j < vertices.size(); ++j) {
             Edge e;
 
-            //e.weight = 0;
+            // 根据操作之间的距离确定权重衰减衰系数(attenuation coefficient) 
+            double ac = 1;
+            intmax_t op_distance = vertices[j].op_no - vertices[i].op_no;
+            ac /= op_distance;
+            cout << i << " " << j << " distance=" << op_distance << " ac=" << ac << endl;
 
             // 如果目标对象相同，那么就在两个操作之间建立一条连接
             if (vertices[i].to_object_addr == vertices[j].to_object_addr) {
                 e.from = i;
                 e.to = j;
-                e.weight = 2.0; // 大于2.0就会导致源、目标对象对调的操作之间的权重设为10的情况下，分不到一组的情况
+                e.weight = 2.0 * ac; // 大于2.0就会导致源、目标对象对调的操作之间的权重设为10的情况下，分不到一组的情况
                 edges.push_back(e);
 
                 e.from = j;
                 e.to = i;
-                e.weight = 2.0;
+                e.weight = 2.0 * ac;
                 edges.push_back(e);
-                continue;
             }
-
             // 如果源对象跟目标对象正好对调，那么就在两个操作之间建立一条连接
-            if (vertices[i].to_object_addr == vertices[j].from_object_addr
+            else if (vertices[i].to_object_addr == vertices[j].from_object_addr
                 and vertices[i].from_object_addr == vertices[j].to_object_addr) {
                 e.from = i;
                 e.to = j;
-                e.weight = 10.0;
+                e.weight = 10.0 * ac;
                 edges.push_back(e);
 
                 e.from = j;
                 e.to = i;
-                e.weight = 10.0;
+                e.weight = 10.0 * ac;
                 edges.push_back(e);
-                continue;
             }
 
 
