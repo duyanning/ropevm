7235b5f3d95dc3861c3d9c4cc8758898a3085efa
Du Yanning <duyanning@gmail.com>
2017-08-23 13:03:03 +0800

    *** empty log message ***

--- tools/Op.h
+++ tools/Op.h
@@ -22,4 +22,45 @@ struct OpEntry {
 };
 
 
+// inline
+// std::ostream&
+// operator<<(std::ostream& strm, const Op& op)
+// {
+//     strm << "story " << story.no << ":" << endl;
+//     strm << "op count: " << story.ops.size() << endl;
+//     for (auto op : story.ops) {
+//         strm << op << " ";
+//     }
+//     strm << endl;
+    
+//     return strm;
+// }
+
+inline
+std::istream&
+operator>>(std::istream& strm, Op& op)
+{
+    string source_class_name;
+    string source_object_addr;
+    string arrow;
+    string target_class_name;
+    string target_object_addr;
+    intmax_t op_no;
+    intmax_t frame_no;
+    intmax_t caller_frame_no;
+
+    strm >> source_class_name >> source_object_addr >> arrow >> target_class_name >> target_object_addr >> op_no
+         >> frame_no >> caller_frame_no;
+
+    op.from_class_name = source_class_name;
+    op.from_object_addr = source_object_addr;
+    op.to_class_name = target_class_name;
+    op.to_object_addr = target_object_addr;
+    op.op_no = op_no;
+    op.frame_no = frame_no;
+    op.caller_frame_no = caller_frame_no;
+
+    return strm;
+}
+
 #endif // OP_H
